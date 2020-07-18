11c2d8e29206f7518685f9b510a299f176ec560c
Ramakant <2016meb1140@iitrpr.ac.in>
2020-07-18 15:48:35 +0530

    set up static files

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
+app.use(express.static('assets'));
 app.use(expressLayouts);
+
+
+//extract styles and scripts from sub pages into layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 //use express router
 app.use('/', require('./routes/index'));
