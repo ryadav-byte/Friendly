1825591a3f88c9bc97cddcd21dfd4bc69be89e44
Ramakant <2016meb1140@iitrpr.ac.in>
2020-07-24 19:25:43 +0530

    convert post related code into AJAX

---
+++ assets/js/home_posts.js
@@ -0,0 +1,142 @@
+{   
+    // method to submit the form data for new post using AJAX
+    let createPost = function(){
+        let newPostForm = $('#new-post-form');
+
+        newPostForm.submit(function(e){
+            e.preventDefault();
+
+            $.ajax({
+                type: 'post',
+                url: '/posts/create',
+                data: newPostForm.serialize(),
+                success: function(data){
+                   let newPost = newPostDom(data.data.post);
+                   $('#posts-list-container').prepend(newPost);
+                   console.log(newPost);
+                   console.log(data);
+                   console.log(data.data.post);
+                }, error: function(error){
+                    console.log(error.responseText);
+                }
+            });
+        });
+    }
+
+
+    // method to create a post in DOM
+    let newPostDom = function(post){
+        return $(`<article id="post-${post._id}">
+        <div id="post-author">
+            <a href="#">
+                <div>
+                    <img
+                        src="https://content.techgig.com/photo/75755485/Are-you-a-programmer-You-can-make-money-with-these-12-ideas-instantly.jpg"
+                        alt="Image"
+                    />
+                    <div>
+                        <div>
+                            <strong id="post-author-name">
+                            ${post.user.name}
+                            </strong
+                            >
+                            <span>
+                                <span>&nbsp;·&nbsp;</span>
+                                1st</span
+                            >
+                        </div>
+                        <span> Software Engineer</span
+                        >
+                        <span>12h</span>
+                    </div>
+                </div>
+            </a>
+            <!-- <div>
+                <span class="fas fa-circle"></span>
+                <span class="fas fa-circle"></span>
+                <span class="fas fa-circle"></span>
+            </div> -->
+            <div>
+                
+                <a class="delete-post-button" href="/posts/destroy/${post._id}"><i class="fas fa-trash"></i></a>
+                
+            </div>
+        </div>
+        <div id="post-data">
+            <p>
+            ${post.content}
+            </p>
+        </div>
+        <div id="post-interactions">
+            <div id="interactions-amount">
+                <span
+                    id="like-icon"
+                    class="fas fa-thumbs-up fa-flip-horizontal"
+                ></span>
+               
+                <span id="amount-info"
+                    >23 <span>&nbsp;·&nbsp;</span> 4
+                    Comments</span
+                >
+            </div>
+            <div id="interactions-btns">
+                <button>
+                    <a style="color: black;" href="#collapseExample${post._id}" role="button" >
+                        <span
+                        class="far fa-thumbs-up fa-flip-horizontal"
+                    ></span>
+                    <span>Like</span>
+                    </a>
+                </button>
+                <button>
+                    <a style="color: black;" data-toggle="collapse" href="#collapseExample${post._id}" role="button" aria-expanded="false" aria-controls="collapseExample">
+                        <span
+                        class="far fa-comment-dots fa-flip-horizontal"
+                    ></span>
+                    <span>Comment</span>
+                    </a>
+                    
+                </button>
+            </div>
+        </div>
+        <p>
+    
+          </p>
+            <div class="collapse" id="collapseExample${post._id}">
+            <form class="form-row" action="comments/create" method="POST">
+                <div class=" mx-sm-3 mb-2 col-10">
+                  
+                  <input type="text" name="content" class="form-control" placeholder="Write a comment here..." required>
+                  <input type="hidden" name="post" value="${post._id}">
+                </div>
+                <button style="background-color:#0091ca;" type="submit" class="btn btn-primary mb-2">Comment</button>
+            </form>
+           
+          </div>
+    </article>`);
+    }
+
+
+    // method to delete a post from DOM
+    let deletePost = function(deleteLink){
+        $(deleteLink).click(function(e){
+            e.preventDefault();
+
+            $.ajax({
+                type: 'get',
+                url: $(deleteLink).prop('href'),
+                success: function(data){
+                    $(`#post-${data.data.post_id}`).remove();
+                    deletePost($(' .delete-post-button', newPost));
+                },error: function(error){
+                    console.log(error.responseText);
+                }
+            });
+
+        });
+    }
+    
+    
+
+    createPost();
+}
