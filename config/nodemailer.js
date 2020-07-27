b4bb8e1ac34d61ace2b6722c1b7800236ef7417b
Ramakant <2016meb1140@iitrpr.ac.in>
2020-07-27 19:25:02 +0530

    Add Delayed Jobs

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -9,7 +9,7 @@ let transporter = nodemailer.createTransport({
     port: 587,
     secure: false,
     auth: {
-        user: '*******', //Your email
+        user: 'rky15798@gmail.com', //Your email
         pass: '*******' // Your password
     }
 });
